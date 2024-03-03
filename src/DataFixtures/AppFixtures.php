896711fd98ad9f2a46549e359e08ec3a208b2438
Morgane Mahé <maeamahe@gmail.com>
2024-03-03 12:11:50 +0100

    30# add entity user & datafixture load & modify security.yaml

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,38 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+    public function __construct(private UserPasswordHasherInterface $userPasswordHasherInterface) {}
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setEmail('admin@site.com');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setFirstname('Admin');
+        $user->setLastname('Admin');
+        $user->setKeyfirst('Mjokjjjspupuzepopjhuoy656');
+        $user->setPathticket('Mjokjjjspupuz/ajepopjhuoy656');
+        $user->setPassword($this->userPasswordHasherInterface->hashPassword($user,'password'));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setEmail('user1@site.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setFirstName('User');
+        $user->setLastName('User');
+        $user->setKeyfirst('Mjokjhuoy656');
+        $user->setPathticket('Mjokjjjspupuz/ajepopjhuoy656');
+        $user->setPassword($this->userPasswordHasherInterface->hashPassword($user,'password'));
+        $manager->persist($user);
+
 
         $manager->flush();
     }
