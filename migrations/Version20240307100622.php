680b493bfe087f7790910af5ecacb6124d23fbbe
Morgane Mahé <maeamahe@gmail.com>
2024-03-07 11:37:33 +0100

    37# add admin

---
+++ migrations/Version20240307100622.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240307100622 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `admin` ADD lastname VARCHAR(60) NOT NULL, ADD firstname VARCHAR(60) NOT NULL, ADD keyfirst VARCHAR(255) NOT NULL, ADD pathticket VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `admin` DROP lastname, DROP firstname, DROP keyfirst, DROP pathticket');
+    }
+}
