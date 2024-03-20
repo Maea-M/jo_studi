03032aab5cdbf56766602eb46e241a29dcab52a2
Morgane Mahé <maeamahe@gmail.com>
2024-03-20 17:30:30 +0100

    add vichuploader config crud evenement to have images

--- src/Controller/Admin/EvenementCrudController.php
+++ src/Controller/Admin/EvenementCrudController.php
@@ -4,9 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\Evenement;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class EvenementCrudController extends AbstractCrudController
 {
@@ -15,14 +18,17 @@ class EvenementCrudController extends AbstractCrudController
         return Evenement::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $mappingsParams = $this->getParameter('vich_uploader.mappings');
+        $moviesImagePath =  $mappingsParams['evenements']['uri_prefix'];
+
+        yield TextField::new('sport', 'Sport');
+        yield DateTimeField::new('date', 'Date');
+        yield TextField::new('location', 'Lieu');
+        yield TextareaField::new('imageFile')->setFormType(VichImageType::class)->hideOnIndex();
+        yield ImageField::new('imageName')->setBasePath($moviesImagePath)->hideOnForm();
     }
-    */
+
 }
