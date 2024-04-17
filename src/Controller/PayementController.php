cd5200c1897f2f557c8628cbd715cf4559d4a3b8
Morgane Mahé <maeamahe@gmail.com>
2024-04-17 18:31:52 +0200

    add success.html and cancel.html/ Modify servces.yaml for bind stripeSK

--- src/Controller/PayementController.php
+++ src/Controller/PayementController.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Controller;
+
+use App\Entity\OrdersDetails;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
 use Stripe\Stripe;
 use Stripe\Checkout\Session;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
 require_once '../vendor/autoload.php';
 require_once '../secret.php';
 
@@ -12,22 +17,57 @@ require_once '../secret.php';
 
 class PayementController extends AbstractController {
 
-    #[Route('/payement', name: 'app_payement')]
-
-    public function __construct(readonly private string $clientSecret) {
-        Stripe::setApiKey($this->$clientSecret);
-        Stripe::setApiVersion(2024-04-10);
+    #[Route('/payement', name: 'payement')]
+    public function index(): Response
+    {
+        return $this->render('payement/index.html.twig', [
+            'controller_name' => 'PayementController',
+        ]);
     }
 
-    public function payement(){
+    /*public function __construct(readonly private string $clientSecret) {
+        Stripe::setApiKey($this->$clientSecret);
+        Stripe::setApiVersion(2024-04-10);
+    }*/
+
+    #[Route('/checkout', name: 'checkout')]
+    public function checkout($stripeSK): Response
+    {
+        Stripe::setApiKey($stripeSK);
         $session = Session::create([
-            'mode' => 'payment',
-            'success_url' => 'http://localhost:8000/success.html',
-            'cancel_url' => 'http://localhost:8000/cancel.html',
-        ]
+            'payment_method_types' => ['card'],
+            'line_items'           => [
+                [
+                    'price_data' => [
+                        'currency'     => 'EUR',
+                        'product_data' => [
+                            'name' => 'T-shirt',
+                        ],
+                        'unit_amount'  => 2000,
+                    ],
+                    'quantity'   => 1,
+                ]
+            ],
+            'mode'                 => 'payment',
+            'success_url'          => $this->generateUrl('success_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url'           => $this->generateUrl('cancel_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
+        ]);
 
-        );
+        return $this->redirect($session->url, 303);
+    }
 
+
+    #[Route('/success-url', name: 'success_url')]
+    public function successUrl(): Response
+    {
+        return $this->render('payement/success.html.twig', []);
+    }
+
+
+    #[Route('/cancel-url', name: 'cancel_url')]
+    public function cancelUrl(): Response
+    {
+        return $this->render('payement/cancel.html.twig', []);
     }
 }
 
