bb32dac4a6092b08eb857c8b8241eaa4f71a6294
Morgane Mahé <maeamahe@gmail.com>
2024-05-03 09:50:40 +0200

    add new test in payement test

--- tests/unit/PayementTest.php
+++ tests/unit/PayementTest.php
@@ -5,24 +5,37 @@ namespace App\Tests\Unit;
 use App\Entity\Payement;
 use App\Entity\Qrcode;
 use App\Entity\OrdersDetails;
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 class PayementTest extends TestCase
 {
-    public function testPayement()
+    public function testAddOrdersDetail()
     {
-        //entité avec des addOrdersDatails et removeOrdersDetails idem avec qr code
+        //entité avec des addOrdersDatails
         $payement = new Payement();
-        $qrcode = new Qrcode();
         $ordersDetails = new OrdersDetails();
 
-        // Test avec OrdersDetails
         $payement->addOrdersDetail($ordersDetails);
         $this->assertTrue($payement->getOrdersDetails()->contains($ordersDetails));
+    }
 
-        // Test avec QR cOde
+    public function testRemoveQrcode()
+    {
+        $payement = new Payement();
+        $qrcode = new Qrcode();
+
         $payement->removeQrcode($qrcode);
-        //$this->assertFalse($qrcode->getPayement()->contains(null));
         $this->assertFalse($payement->getQrcodes()->contains($qrcode));
     }
+    
+    public function testFailedRemoveQrcode()
+    {
+        $this->expectException(InvalidArgumentException::class);
+    
+        $payement = new Payement();
+        $qrcode = new Qrcode();
+
+        $payement->removeQrcode($qrcode);
+    }        
 }
